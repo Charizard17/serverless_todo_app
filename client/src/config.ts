a68539c65709d3b834c418170f0a7094ed6af26e
Esad Dursun <3723esad@gmail.com>
2022-11-07 00:30:28 +0100

    client config file edited

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = ''
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+const apiId = "yb9ttux9cb";
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/prod`;
 
 export const authConfig = {
-  domain: '',
-  clientId: '',
-  callbackUrl: 'http://localhost:3000/callback'
-}
+  domain: "dev-4ikc5lybrkey36cg.us.auth0.com",
+  clientId: "oWLfge9LeaKZw6llvCKN4H5pg9LEWpDl",
+  callbackUrl: "http://localhost:3000/callback",
+};
