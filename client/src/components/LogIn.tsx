f291848966e79754f73f626f5fe3657d414ef784
Esad Dursun <3723esad@gmail.com>
2022-11-07 08:12:53 +0100

    frontend client changes

--- client/src/components/LogIn.tsx
+++ client/src/components/LogIn.tsx
@@ -1,27 +1,27 @@
-import * as React from 'react'
-import Auth from '../auth/Auth'
-import { Button } from 'semantic-ui-react'
+import * as React from "react";
+import Auth from "../auth/Auth";
+import { Button } from "semantic-ui-react";
 
 interface LogInProps {
-  auth: Auth
+  auth: Auth;
 }
 
 interface LogInState {}
 
 export class LogIn extends React.PureComponent<LogInProps, LogInState> {
   onLogin = () => {
-    this.props.auth.login()
-  }
+    this.props.auth.login();
+  };
 
   render() {
     return (
       <div>
-        <h1>Please log in</h1>
+        <h1>Please Login</h1>
 
         <Button onClick={this.onLogin} size="huge" color="olive">
-          Log in
+          Login using AuthO
         </Button>
       </div>
-    )
+    );
   }
 }
